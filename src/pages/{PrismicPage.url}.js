d893c1a0944f1115fae9536730140c0a2fc9de92
Neil iMac <nmastroianni@longbranch.k12.nj.us>
2021-08-24 14:59:04 -0400

    adding dynamic pages

---
+++ src/pages/{PrismicPage.url}.js
@@ -0,0 +1,36 @@
+import * as React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/Layout"
+import Seo from "../components/Seo"
+import SliceZone from "../components/SliceZone"
+
+export default function Page({ data, path }) {
+  if (!data) return null
+  const document = data.page.data
+  return (
+    <Layout path={path}>
+      <Seo title={document.page_title.text} />
+      <div>
+        <SliceZone sliceZone={document.body} />
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query PageQuery($id: String) {
+    page: prismicPage(id: { eq: $id }) {
+      data {
+        page_title {
+          text
+        }
+        body {
+          ... on PrismicSliceType {
+            slice_type
+          }
+          ...PageDataBodySectionHeading
+        }
+      }
+    }
+  }
+`
---
+++ src/pages/{PrismicPost.url}.js
@@ -0,0 +1,36 @@
+import * as React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/Layout"
+import Seo from "../components/Seo"
+import SliceZone from "../components/SliceZone"
+
+export default function Post({ data, path }) {
+  if (!data) return null
+  const document = data.post.data
+  return (
+    <Layout path={path}>
+      <Seo title={document.post_title.text} />
+      <div>
+        <SliceZone sliceZone={document.body} />
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query PostQuery($id: String) {
+    post: prismicPost(id: { eq: $id }) {
+      data {
+        post_title {
+          text
+        }
+        body {
+          ... on PrismicSliceType {
+            slice_type
+          }
+          ...PostDataBodySectionHeading
+        }
+      }
+    }
+  }
+`
