9046caafde2cd675b316ea4f891cca262c6a39b1
Neil iMac <nmastroianni@longbranch.k12.nj.us>
2021-08-31 10:56:12 -0400

    centering button on mobile

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,8 +1,10 @@
 import * as React from "react"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import { Link } from "gatsby"
 
 export default function Hero({
   bannerDescription,
+  bannerLink,
   bannerLinkLabel,
   bannerTitle,
   heroImage,
@@ -16,16 +18,19 @@ export default function Hero({
           alt={alt ? alt : `Decorative Image`}
           className="w-4/5 md:w-3/4 lg:w-4/5 block mx-auto"
         />
-        <div className=" dark:text-white my-4 md:my-0">
+        <div className=" dark:text-white my-4 text-center md:text-left">
           <h2 className="text-2xl sm:text-3xl md:text-5xl lg:text-7xl text-center md:text-left text-green-800 dark:text-green-200 font-teko">
             {bannerTitle}
           </h2>
-          <p className="text-base sm:text-lg lg:text-xl xl:text-2xl mb-3 md:mb-6 lg:max-w-3xl">
+          <p className="text-base text-left sm:text-lg lg:text-xl xl:text-2xl mb-3 md:mb-6 lg:max-w-3xl">
             {bannerDescription}
           </p>
-          <button className="px-4 py-3 border-2 text-green-800 dark:text-green-200 border-green-800 dark:border-green-400 rounded-md hover:shadow-md hover:bg-green-200 hover:bg-opacity-20 dark:hover:bg-black dark:hover:bg-opacity-70 focus:outline-none focus:ring-4 focus:ring-green-300">
+          <Link
+            to={bannerLink ? bannerLink : "/help"}
+            className="inline-block px-4 py-3 my-3 border-2 text-green-800 dark:text-green-200 border-green-800 dark:border-green-400 rounded-md hover:shadow-md hover:bg-green-200 hover:bg-opacity-20 dark:hover:bg-black dark:hover:bg-opacity-70 focus:outline-none focus:ring-4 focus:ring-green-300"
+          >
             {bannerLinkLabel}
-          </button>
+          </Link>
         </div>
       </div>
     </>
