e960bff977d027b78f14a0be703cdf721808e9e1
Neil iMac <nmastroianni@longbranch.k12.nj.us>
2021-09-09 13:54:46 -0400

    moving updated text

--- src/pages/{PrismicTool.url}.js
+++ src/pages/{PrismicTool.url}.js
@@ -14,23 +14,25 @@ const PrismicTool = ({ data, path }) => {
     <Layout path={path}>
       <Seo title={document.tool_title.text} />
       <header>
-        <div className="flex justify-center items-center flex-wrap md:divide-x-4 divide-green-700 py-6">
+        <div className="flex flex-col justify-center items-center md:flex-row md:divide-x-4 divide-green-700 dark:divide-green-200 py-6">
           <GatsbyImage
             image={getImage(document.tool_logo.gatsbyImageData)}
             alt={document.tool_logo.alt || "Tool Logo"}
             className="col-span-1 text-center rounded w-96 mr-0 md:mr-12"
             imgClassName="rounded"
           />
-          <h1 className="text-center pl-0 md:pl-12  w-96 text-6xl text-green-800 dark:text-green-200 font-teko">
-            {document.tool_title.text}
-          </h1>
+          <div className="my-4 md:my-0">
+            <h1 className="text-center pl-0 md:pl-12  w-96 text-6xl text-green-800 dark:text-white font-teko">
+              {document.tool_title.text}
+            </h1>
+            <p className="text-center pl-0 md:pl-12 font-source text-green-700 dark:text-green-200">
+              Last Updated:
+            </p>
+            <p className="pl-0 md:pl-12 mb-3 md:mb-4 lg:mb-6 text-center text-green-700 dark:text-green-200">
+              {data.tool.last_publication_date}
+            </p>
+          </div>
         </div>
-        <p className="text-center font-teko text-lg text-green-700 dark:text-green-300">
-          Last Updated:
-        </p>
-        <h2 className="mb-3 md:mb-4 lg:mb-6 font-teko text-center text-lg text-green-700 dark:text-green-300">
-          {data.tool.last_publication_date}
-        </h2>
       </header>
       <SliceZone sliceZone={document.body} />
     </Layout>
