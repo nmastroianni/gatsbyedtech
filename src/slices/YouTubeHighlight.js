59b86d4f995943c1921457fe46fcd004250af697
Neil iMac <nmastroianni@longbranch.k12.nj.us>
2021-09-16 15:07:34 -0400

    adding layout structure

--- src/slices/YouTubeHighlight.js
+++ src/slices/YouTubeHighlight.js
@@ -1,17 +1,20 @@
 import * as React from "react"
 import { graphql } from "gatsby"
 import { RichText } from "prismic-reactjs"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import htmlSerializer from "../utils/htmlSerializer"
 
 export const YouTubeHighlight = ({ slice }) => {
   const {
     primary: {
+      youtube_description,
       youtube_embed: { embed_url, provider_name, thumbnail_url, title },
       youtube_position,
-      youtube_thumbnail: { alt, gatsbyImageData },
-      youtube_title: { raw },
+      youtube_thumbnail,
+      youtube_title,
     },
   } = slice
+  // Get the proper video data based on the video source
   let video
   let videoPosition
   if (provider_name === "YouTube") {
@@ -24,11 +27,79 @@ export const YouTubeHighlight = ({ slice }) => {
     video = embed_url
   }
   youtube_position ? (videoPosition = "right") : (videoPosition = "left")
+  // Conditionally return/render content based on videoPosition value
   if (videoPosition === "left") {
     return (
-      <div className="max-w-screen-2xl mx-auto bg-purple-200">
-        <RichText render={raw} htmlSerializer={htmlSerializer} />
-      </div>
+      <section className="">
+        <div className="max-w-screen-2xl mx-auto grid md:grid-cols-2 p-4 gap-y-4 divide-y md:divide-y-0 md:gap-x-4">
+          {/* BEGIN LEFT SIDE VIDEO EMBED */}
+          <div className="bg-black p-3 rounded-md">
+            <div className="bg-gradient-to-b from-black to-gray-900 aspect-w-16 aspect-h-9 rounded-md">
+              {provider_name !== "video.other" && (
+                <iframe
+                  title={RichText.asText(youtube_title.raw)}
+                  className="rounded-md"
+                  src={video}
+                  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                  loading="lazy"
+                  allowFullScreen
+                  srcDoc={`<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style>
+                        <a href=${video}>
+                          <img
+                            src=${youtube_thumbnail.url}
+                            alt='Decortaive' />
+                          <span>&#x25BA;</span>
+                          ${
+                            youtube_thumbnail.alt && (
+                              <GatsbyImage
+                                image={getImage(
+                                  youtube_thumbnail.gatsbyImageData
+                                )}
+                              />
+                            )
+                          }
+                        </a>`}
+                ></iframe>
+              )}
+              {provider_name === "video.other" && (
+                <iframe
+                  src={video}
+                  className="mx-auto rounded-md"
+                  allow="autoplay"
+                  title={RichText.asText(youtube_title.raw)}
+                ></iframe>
+              )}
+            </div>
+          </div>
+          {/* END LEFTSIDE VIDEO EMBED */}
+          {/* BEGIN RIGHT SIDE VIDEO DESCRIPTION */}
+          <div className="w-full pt-4 mx-auto">
+            <div className="prose lg:prose-xl dark:prose-dark">
+              <RichText
+                render={youtube_title.raw}
+                htmlSerializer={htmlSerializer}
+              />
+              <div className="h-1 rounded bg-gradient-to-r dark:from-transparent dark:via-green-400 dark:to-transparent" />
+              <RichText
+                render={youtube_description.raw}
+                htmlSerializer={htmlSerializer}
+              />
+            </div>
+          </div>
+          {/* END RIGHT SIDE VIDEO DESCRIPTION */}
+        </div>
+      </section>
+    )
+  } else {
+    return (
+      <section className="">
+        <div className="max-w-screen-2xl mx-auto grid grid-cols-1 md:grid-cols-2 justify-items-center items-center bg-purple-200">
+          <RichText
+            render={youtube_description.raw}
+            htmlSerializer={htmlSerializer}
+          />
+        </div>
+      </section>
     )
   }
 }
@@ -37,6 +108,9 @@ export const query = graphql`
   fragment HomepageDataBodyYoutubeHighlight on PrismicHomepageDataBodyYoutubeHighlight {
     id
     primary {
+      youtube_description {
+        raw
+      }
       youtube_embed {
         embed_url
         provider_name
@@ -45,8 +119,7 @@ export const query = graphql`
       }
       youtube_position
       youtube_thumbnail {
-        alt
-        gatsbyImageData(placeholder: BLURRED)
+        url
       }
       youtube_title {
         raw
