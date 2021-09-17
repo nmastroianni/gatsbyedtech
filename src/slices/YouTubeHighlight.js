27c14a645d74ad7014e188cf7e4e4c12428dcfcf
Neil iMac <nmastroianni@longbranch.k12.nj.us>
2021-09-17 08:34:38 -0400

    adding position right

--- src/slices/YouTubeHighlight.js
+++ src/slices/YouTubeHighlight.js
@@ -14,7 +14,6 @@ export const YouTubeHighlight = ({ slice }) => {
       youtube_title,
     },
   } = slice
-  console.log(getSrcSet(youtube_thumbnail.gatsbyImageData))
   // Get the proper video data based on the video source
   let video
   let videoPosition
@@ -70,12 +69,11 @@ export const YouTubeHighlight = ({ slice }) => {
           {/* END LEFTSIDE VIDEO EMBED */}
           {/* BEGIN RIGHT SIDE VIDEO DESCRIPTION */}
           <div className="w-full pt-4 mx-auto">
-            <div className="prose lg:prose-xl dark:prose-dark">
-              <RichText
-                render={youtube_title.raw}
-                htmlSerializer={htmlSerializer}
-              />
-              <div className="h-1 rounded bg-gradient-to-r dark:from-transparent dark:via-green-400 dark:to-transparent" />
+            <h2 className="text-center text-green-800 dark:text-green-200 font-teko text-3xl md:text-4xl">
+              {RichText.asText(youtube_title.raw)}
+            </h2>
+            <div className="h-1 my-3 rounded bg-gradient-to-r from-transparent via-green-800 to-transparent dark:from-transparent dark:via-green-400 dark:to-transparent" />
+            <div className="prose prose-green lg:prose-xl dark:prose-dark">
               <RichText
                 render={youtube_description.raw}
                 htmlSerializer={htmlSerializer}
@@ -89,11 +87,57 @@ export const YouTubeHighlight = ({ slice }) => {
   } else {
     return (
       <section className="">
-        <div className="max-w-screen-2xl mx-auto grid grid-cols-1 md:grid-cols-2 justify-items-center items-center bg-purple-200">
-          <RichText
-            render={youtube_description.raw}
-            htmlSerializer={htmlSerializer}
-          />
+        <div className="max-w-screen-2xl mx-auto grid md:grid-cols-2 p-4 gap-y-4 divide-y md:divide-y-0 md:gap-x-4">
+          {/* BEGIN LEFT SIDE VIDEO DESCRIPTION */}
+          <div className="w-full pt-4 mx-auto">
+            <h2 className="text-center text-green-800 dark:text-green-200 font-teko text-3xl md:text-4xl">
+              {RichText.asText(youtube_title.raw)}
+            </h2>
+            <div className="h-1 my-3 rounded bg-gradient-to-r from-transparent via-green-800 to-transparent dark:from-transparent dark:via-green-400 dark:to-transparent" />
+            <div className="prose prose-green lg:prose-xl dark:prose-dark">
+              <RichText
+                render={youtube_description.raw}
+                htmlSerializer={htmlSerializer}
+              />
+            </div>
+          </div>
+          {/* END LEFTSIDE VIDEO DESCRIPTION */}
+          {/* BEGIN RIGHT SIDE VIDEO EMBED */}
+          <div className="bg-black p-3 rounded-md">
+            <div className="bg-gradient-to-b from-black to-gray-900 aspect-w-16 aspect-h-9 rounded-md">
+              {provider_name !== "video.other" && (
+                <iframe
+                  title={RichText.asText(youtube_title.raw)}
+                  className="rounded-md"
+                  src={video}
+                  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                  loading="lazy"
+                  allowFullScreen
+                  srcDoc={`<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style>
+                        <a href=${video}>
+                          <img
+                            src=${youtube_thumbnail.url}
+                            srcset=${getSrcSet(
+                              youtube_thumbnail.gatsbyImageData
+                            )}
+                            alt='Decortaive' />
+                          <span>&#x25BA;</span>
+                          
+                        </a>`}
+                ></iframe>
+              )}
+              {provider_name === "video.other" && (
+                <iframe
+                  src={video}
+                  className="mx-auto rounded-md"
+                  allow="autoplay"
+                  title={RichText.asText(youtube_title.raw)}
+                ></iframe>
+              )}
+            </div>
+          </div>
+
+          {/* END RIGHT SIDE VIDEO EMBED */}
         </div>
       </section>
     )
