66b5901d08670b0b59c2484d19ed801e96a6c606
Neil iMac <nmastroianni@longbranch.k12.nj.us>
2021-09-17 13:20:54 -0400

    conditional srcSet

--- src/slices/YouTubeHighlight.js
+++ src/slices/YouTubeHighlight.js
@@ -50,14 +50,16 @@ export const YouTubeHighlight = ({ slice }) => {
                   srcDoc={`<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style>
                         <a href=${video}>
                           <img
-                            src=${
+                            src='${
                               youtube_thumbnail.url
                                 ? youtube_thumbnail.url
                                 : thumbnail_url
-                            }
-                            srcset=${getSrcSet(
+                            }'
+                            srcset='${
                               youtube_thumbnail.gatsbyImageData
-                            )}
+                                ? getSrcSet(youtube_thumbnail.gatsbyImageData)
+                                : ``
+                            }'
                             alt='Decortaive' />
                           <span>&#x25BA;</span>
                           
