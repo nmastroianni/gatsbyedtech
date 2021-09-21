269fc10fd8e75e65d8e95b202ebb9ec068c7a68e
Neil iMac <nmastroianni@longbranch.k12.nj.us>
2021-09-21 15:15:56 -0400

    fixing default thumbnails on right

--- src/slices/YouTubeHighlight.js
+++ src/slices/YouTubeHighlight.js
@@ -5,6 +5,7 @@ import { getSrcSet } from "gatsby-plugin-image"
 import htmlSerializer from "../utils/htmlSerializer"
 
 export const YouTubeHighlight = ({ slice }) => {
+  console.log(slice)
   const {
     primary: {
       youtube_description,
@@ -130,10 +131,16 @@ export const YouTubeHighlight = ({ slice }) => {
                   srcDoc={`<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style>
                         <a href=${video}>
                           <img
-                            src=${youtube_thumbnail.url}
-                            srcset=${getSrcSet(
+                            src='${
+                              youtube_thumbnail.url
+                                ? youtube_thumbnail.url
+                                : thumbnail_url
+                            }'
+                            srcset='${
                               youtube_thumbnail.gatsbyImageData
-                            )}
+                                ? getSrcSet(youtube_thumbnail.gatsbyImageData)
+                                : ``
+                            }'
                             alt='Decortaive' />
                           <span>&#x25BA;</span>
                           
