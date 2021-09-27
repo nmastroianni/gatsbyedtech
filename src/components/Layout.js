5e1be31cbc5c69447ff9ba60f74637fd06053115
Neil iMac <nmastroianni@longbranch.k12.nj.us>
2021-09-27 14:15:46 -0400

    adding tracking consent

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,42 @@
 import * as React from "react"
+import { useEffect } from "react"
+import { Link } from "gatsby"
 import Navbar from "./Navbar"
 import Footer from "./Footer"
 import { BsArrowReturnLeft } from "react-icons/bs"
+import { HiThumbDown, HiThumbUp } from "react-icons/hi"
 
 export default function Layout({ children, path }) {
+  useEffect(() => {
+    const consentModal = document.querySelector("#tracking-consent")
+    setTimeout(() => {
+      consentModal.classList.add("opacity-100")
+    }, 1200)
+  })
   return (
-    <>
+    <div className="relative">
+      <div
+        id="tracking-consent"
+        className="fixed opacity-0 transition duration-700 ease-in-out bg-gray-200 dark:bg-gray-700 rounded p-3 md:p-4 lg:p-6 w-full md:w-1/2 lg:w-1/3 left-1/2 transform -translate-x-1/2 top-10 z-50"
+      >
+        <header className="prose dark:prose-dark">
+          This site uses cookies to measure how visitors use our site.
+        </header>
+        <div className="text-xs text-green-800 dark:text-green-200 mb-3 md:mb-4 lg:mb-6">
+          <Link to="/privacy">Learn more</Link>
+        </div>
+        <div className="grid grid-cols-2 text-green-800 dark:text-green-200">
+          <button className="px-2 py-3 border border-green-800 dark:border-green-400 transition duration-300 ease-in-out dark:hover:text-green-300 hover:bg-green-200 dark:hover:bg-gray-800 hover:bg-opacity-50 rounded mr-3">
+            <HiThumbDown className="inline" /> Decline
+          </button>
+          <button
+            id="accept-tracking"
+            className="px-2 py-3 border border-green-800 dark:border-green-400 transition duration-300 ease-in-out dark:hover:text-green-200 hover:bg-green-100 dark:hover:bg-gray-600 hover:bg-opacity-50 rounded ml-3"
+          >
+            <HiThumbUp className="inline" /> Accept
+          </button>
+        </div>
+      </div>
       <ul id="nav-access" className="relative mx-auto">
         <li>
           <a
@@ -27,6 +58,6 @@ export default function Layout({ children, path }) {
         <main id="main-content">{children}</main>
         <Footer />
       </div>
-    </>
+    </div>
   )
 }
