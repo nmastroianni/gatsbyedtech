af7612919356adc193e4b11c7bb788655df2ee55
Neil iMac <nmastroianni@longbranch.k12.nj.us>
2021-10-08 14:34:52 -0400

    attempting fix double trailing slash canonical

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-react-helmet-canonical-urls`,
       options: {
-        siteUrl: `https://edtechwave.com/`,
+        siteUrl: `https://edtechwave.com`,
       },
     },
     {
