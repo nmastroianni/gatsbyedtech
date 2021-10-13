423488c69401a9800b6809a663871c9fd8c91871
Neil iMac <nmastroianni@longbranch.k12.nj.us>
2021-10-13 11:14:23 -0400

    conditionally render link copied

--- src/slices/SectionHeading.js
+++ src/slices/SectionHeading.js
@@ -20,26 +20,31 @@ export const SectionHeading = ({ slice }) => {
       style={{ backgroundColor: `${section_heading_background_color}` }}
       id={section_heading_link.text}
     >
-      <p
-        className={` z-10  text-white absolute bottom-0 transition duration-300 ease-in-out ${
-          isCopied ? " opacity-80 " : " opacity-0"
-        }`}
-      >
-        Link Copied!
-      </p>
-      <button
-        className="text-green-900 text-opacity-0 dark:text-opacity-0 group-hover:text-opacity-100 focus:text-white focus:text-opacity-100 transition duration-300 ease-in-out group-hover:text-white inline"
-        onClick={() => {
-          setIsCopied(true)
-          navigator.clipboard.writeText(`${url}#${section_heading_link.text}`)
-          setTimeout(() => {
-            setIsCopied(false)
-          }, 1000)
-        }}
-      >
-        <HiLink className="h-4 w-4 md:h-8 md:w-8 mx-1" />
-        <span className="sr-only">Copy heading link</span>
-      </button>
+      <div className="relative">
+        {isCopied ? (
+          <p
+            className={` z-10 w-24 text-white absolute -top-6 -left-6 
+          }`}
+          >
+            Link Copied!
+          </p>
+        ) : (
+          ""
+        )}
+        <button
+          className="relative text-green-900 text-opacity-0 dark:text-opacity-0 group-hover:text-opacity-100 focus:text-white focus:text-opacity-100 transition duration-300 ease-in-out group-hover:text-white inline"
+          onClick={() => {
+            setIsCopied(true)
+            navigator.clipboard.writeText(`${url}#${section_heading_link.text}`)
+            setTimeout(() => {
+              setIsCopied(false)
+            }, 1000)
+          }}
+        >
+          <HiLink className="h-4 w-4 md:h-8 md:w-8 mx-1" />
+          <span className="sr-only">Copy heading link</span>
+        </button>
+      </div>
       <h2 className="text-white text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-teko inline">
         {RichText.asText(section_heading_text.raw)}
       </h2>
