2fdce50604c53ec70a1d68ff5541c2c34721c348
Neil Mastroianni <nmastroianni@longbranch.k12.nj.us>
2022-10-13 07:44:09 -0400

    add redirect

--- src/templates/challenges-index.js
+++ src/templates/challenges-index.js
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { graphql, Link, navigate } from "gatsby"
+import { graphql, Link } from "gatsby"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import Seo from "../components/Seo"
 import Layout from "../components/Layout"
